c8dd50c9bb88bf12ff5301a94e95b6ed1056d3d1
jangot <pulin.pavel@gmail.com>
2014-10-13 00:11:17 +0400

    first

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+defined('_JEXEC') or die;
+JHtml::_('behavior.framework', true);
+$app = JFactory::getApplication();
+?>
+<?php echo '<?'; ?>xml version="1.0" encoding="<?php echo $this->_charset ?>"?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
